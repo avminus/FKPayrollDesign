be73e8f6d8365da2c20508e52a65665a885f7fe6
Arjun Dhar <avminus@gmail.com>
2020-05-11 04:14:44 +0530

    Added the add employee feature. And also tested the code by printing the database after the insertion of new employee

--- SourceCode/DBOperations.java
+++ SourceCode/DBOperations.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.time.*;
 
 public class DBOperations{
-	public ArrayList<EmployeeInterface >makedatabase(){
+	public static ArrayList<EmployeeInterface > makedatabase(){
 		ArrayList<EmployeeInterface > dblist = new ArrayList <EmployeeInterface >();
 
 		dblist.add(new MonthlyEmployee(1, 50000d, MOP.Bank_Payment, 50d, 0d,new NoneUnion(),LocalDate.of(2020, Month.MAY, 8) ) );
@@ -14,7 +14,20 @@ public class DBOperations{
 
 		return dblist;
 	}
-		
+	static void display(ArrayList<EmployeeInterface > dblist){
+		for(EmployeeInterface bobj: dblist){
+			// if(bobj instanceof MonthlyEmployee){
+			// 	MonthlyEmployee obj = (MonthlyEmployee) bobj;
+			// 	System.out.println(obj.id + " "+ obj.+" "+ obj.lastpaid);
+			// }
+			// else{
+			// 	HourlyEmployee obj = (HourlyEmployee) bobj;
+			// 	System.out.println(obj.id + " "+ obj.hourlyrate+" "+ obj.lastpaid);
+			// }
+			System.out.println(bobj);
+		}
+	}
+
 	static void addtoDB(){
 
 	}
--- SourceCode/FKPayrollDesign.java
+++ SourceCode/FKPayrollDesign.java
@@ -1,12 +1,78 @@
 package trainingproject;
 
 import java.util.*;
+import java.time.*;
 
 public class FKPayrollDesign{
+ 	static void addemployee(ArrayList<EmployeeInterface> dblist){
+
+			System.out.println("What type of imployee you want? Press 1 for Monthly and 0 for Hourly.");
+			Scanner sc = new Scanner(System.in);
+			int temp  = sc.nextInt();
+			EmployeeInterface impobj;
+			int id = dblist.size()+1;
+			if(temp == 1){
+				System.out.println("Enter the Monthly salary.");	
+				double salary = sc.nextDouble();
+				System.out.println("Enter the method of payment! Press 0 for BankPayment, 1 for Postal and 2 for PayMaster");
+				int payment= sc.nextInt();
+				System.out.println("Enter the comission rate of sales for the employee!");
+				double rate = sc.nextDouble();
+				System.out.println("Is the employee part of Union? Press 1 for yes and 0 for No");
+				int decision = sc.nextInt();
+				UnionInterface unionobj;
+				if(decision==1){
+					System.out.println("Enter the weekly due for Union");
+					unionobj = new FirstUnion(sc.nextDouble());
+				} 
+				else
+					unionobj = new NoneUnion();
+				LocalDate daty = LocalDate.now();
+
+				impobj = new MonthlyEmployee(id, salary, MOP.values()[payment],rate, 0d, unionobj, daty );	
+			}
+			else{
+				System.out.println("Enter the hourly rate.");	
+				double hourrate = sc.nextDouble();
+				System.out.println("Enter the method of payment! Press 0 for BankPayment, 1 for Postal and 2 for PayMaster");
+				int payment= sc.nextInt();
+				System.out.println("Is the employee part of Union? Press 1 for yes and 0 for No");
+				int decision = sc.nextInt();
+				UnionInterface unionobj;
+				if(decision==1){
+					System.out.println("Enter the weekly due for Union");
+					unionobj = new FirstUnion(sc.nextDouble());
+				} 
+				else
+					unionobj = new NoneUnion();
+				LocalDate daty = LocalDate.now();
+				impobj = new HourlyEmployee(id, hourrate, MOP.values()[payment], 0d, 0d, unionobj, daty);
+			}
+			dblist.add(impobj);
+	}
+
+	static void deleteemployee(){
+
+	}
+
+	static void submittimecard(){
+		// System.out.println()
+	}
 	public static void main(String args[]){
 
-		System.out.println("Added the classes. Working fine.");
+		System.out.println("Do you want to add Employees?: Enter 1 for Yes and 0 for No!");
 
-		System.out.println("Added the Database. Working fine.");		
+		Scanner sc  = new Scanner(System.in);
+
+		int temp = sc.nextInt();
+
+		ArrayList<EmployeeInterface> dblist = DBOperations.makedatabase();
+
+		if(temp==1){
+			addemployee(dblist);
+			System.out.println("Added an employee successfully");	
+		}
+		DBOperations.display(dblist);
+		// System.out.println("Do you want to delete an employee? Press 1 for Yes and 0 for No!");		
 	}
 }
