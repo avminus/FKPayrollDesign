120608ffd2bfbeecaf8a6f232b0d72850ed899a2
Arjun Dhar <35273797+avminus@users.noreply.github.com>
2020-05-11 02:21:12 +0530

    Laid the foundation
    Implemented all the use case Classes and Interfaces and a dummy driver function.

---
+++ FKPayrollDesign.java
@@ -0,0 +1,10 @@
+package trainingproject;
+
+import java.util.*;
+
+public class FKPayrollDesign{
+	public static void main(String args[]){
+
+		System.out.println("Added the classes. Working fine.");
+	}
+}
